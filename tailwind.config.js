622a1dbb28ca666bcac576e46d1daa35d9b91abd
chukwujekwuabrahamthebest247-hash <chukwujekwuabrahamthebest247@gmail.com>
2026-01-09 09:53:13 +0100

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+```javascript
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
